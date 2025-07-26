12c415cea88fc26dc5dcdb25a7deff2f257c6b2e
Brandon Caudillo <btcaudillo@gmail.com>
2025-07-26 11:51:57 -0600

    fixed import issue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // Import Tailwind CSS
-import App from './App';
+import App from './app.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
